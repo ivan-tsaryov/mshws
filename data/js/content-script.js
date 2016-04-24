9a6ff1ee6135ffc49dc63929275d2bd7c008ccc6
Ivan Tsaryov <ivan.tsaryov@gmail.com>
2016-04-24 11:22:44 +0400

    Version 0.0.1

--- data/js/content-script.js
+++ data/js/content-script.js
@@ -1,5 +1,12 @@
 self.port.on("click", function(comments) {
+    /*
     $("div[data-comment-id]").eq(0).removeAttr("data-comment-id");
     $("div[data-comment-id]").eq(0).removeAttr("data-comment-id");
     $(window).scrollTop($('div[data-comment-id]').offset().top);
+    */
+    
+    var $el = $("div[class~='_hint']").eq(0);
+    $("div[class~='_hint']").eq(0).attr("class", $el.attr("class").replace("_hint", ""));
+    
+    $(window).scrollTop( $("div[class~='_hint']").eq(0).offset().top);
 });
